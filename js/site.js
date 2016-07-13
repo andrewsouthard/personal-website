bf4e38685231615861869eac5f85e918900d5197
Andrew Southard <andrewmsouthard@gmail.com>
2016-07-13 16:26:09 -0400

    Updated layout so it is now responsive and added content for all sections.

--- js/site.js
+++ js/site.js
@@ -1,19 +1,33 @@
-/*
- * Offset the scrollspy so it will correctly highlight the link in focus. 
- */
-$('body').scrollspy({ target: '#navbar', offset: 400, })
+/* Set the width where the behavior will change for smaller screens. */
+var minWidth = 1200;
 
-/*
- * Smoothly scroll to the anchor when it is clicked.
- */
+/* Offset the scrollspy so it will correctly highlight the link in focus. */
+$('body').scrollspy({ target: '#navbar', offset: $(window).height()/2, });
+
+/* Smoothly scroll to the anchor when it is clicked. */
 $("a").click(function(event) {
-    if($.attr(this, 'href').match("^#")) {
-        var offset = 210;
+
+    /* Only run this if the link is an anchor and the screen width is large. */
+    if($.attr(this, 'href').match("^#") && $(window).width() > minWidth) {
+
+        /* Prevent the default action of going to the anchor. */
         event.preventDefault();
+
+        /*
+         * Calculate the offset from the top of the page where the top of the
+         * anchor should be placed. The div should be centered vertically in
+         * the window.
+         */
+        var offset = $(window).height()/2 - $($.attr(this, 'href')).height()/2;
         var anchor = $($.attr(this, 'href')).offset().top - offset;
-        if(anchor < 200) {
-            anchor = 0;
-        }
+
+        /* Scroll to the middle of the div. */
         $('html, body').animate({scrollTop: anchor}, 500);
+    }
+});
+$(document).ready(function(){
+    /* Set the top margin of the first box based on the window size. */
+    if($(window).width() > minWidth) {
+        $('.content-box').css({'margin-top': $(window).height()/3 });;
     }
 });
