16ce55eefb43895b7f75cb1abc5c7a7b3b44f8db
Andrew Southard <andrewmsouthard@gmail.com>
2016-07-26 16:54:22 -0400

    Updates to optimize for SEO, cleanup on mobile views.

--- js/site.js
+++ js/site.js
@@ -7,8 +7,8 @@ $('body').scrollspy({ target: '#navbar', offset: $(window).height()/2, });
 /* Smoothly scroll to the anchor when it is clicked. */
 $("a").click(function(event) {
 
-    /* Only run this if the link is an anchor and the screen width is large. */
-    if($.attr(this, 'href').match("^#") && $(window).width() > minWidth) {
+    /* Only run this if the link is an anchor. */
+    if($.attr(this, 'href').match("^#")) {
 
         /* Prevent the default action of going to the anchor. */
         event.preventDefault();
@@ -16,9 +16,14 @@ $("a").click(function(event) {
         /*
          * Calculate the offset from the top of the page where the top of the
          * anchor should be placed. The div should be centered vertically in
-         * the window.
+         * the window unless this is a small screen. In that case, the div
+         * should be at the top of the window.
          */
-        var offset = $(window).height()/2 - $($.attr(this, 'href')).height()/2;
+        if($(window).width < minWidth) {
+            var offset = 0;
+        } else {
+            var offset = $(window).height()/2 - $($.attr(this, 'href')).height()/2;
+        }
         var anchor = $($.attr(this, 'href')).offset().top - offset;
 
         /* Scroll to the middle of the div. */
